07431cee821e11e239e8d0373e8404ca0e4ebb32
razmy <razmyslovichv@gmail.com>
2021-10-23 20:13:29 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,15 +3,6 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        // String s = "Merry";
-        // System.out.println("Christmas");
-        // System.out.println("Merry New");
-        // System.out.println("weekend");
-        // System.out.print("Happy New");
-        // System.out.println("weekend");
-        // System.out.println(s);
-        // System.out.print("Merry New!");
-        // System.out.print(" ");
-        // System.out.println("Year");
+        System.out.println("Main.main");
     }
 }
